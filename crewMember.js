56bc5c955875af7e1b93e3a3419d344513a4df7f
dina aziz <dina.aziz100@yahoo.com>
2017-03-18 17:34:08 +0000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,30 @@
 
+
+function CrewMember (position) {
+  this.position = position;
+  this.currentShip = 'Looking for a Rig';
+}
+
+CrewMember.prototype.engageWarpDrive = function(){
+  if (this.position === 'Pilot' && typeof(this.currentShip) !== 'string')
+  	{ this.currentShip.warpDrive ='engaged!';}
+  else{
+    return 'had no effect';
+  }
+}
+CrewMember.prototype.setsInvisibility = function(){
+  if (this.position === 'Defender' && typeof(this.currentShip) !== 'string') {
+    this.currentShip.cloaked = true;
+  }
+  else{
+    return 'had no effect';
+  }
+}
+CrewMember.prototype.chargePhasers = function(){
+  if (this.position === 'Gunner' && typeof(this.currentShip) !== 'string') {
+    this.currentShip.phasersCharge = 'charged!';
+  }
+  else{
+    return 'had no effect';
+  }
+}
